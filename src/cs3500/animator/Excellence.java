ae4b8c0ee316ef2589e67987095bd43ee1c3f32f
Ramzi Talhouk <talhouk.r@husky.neu.edu>
2019-06-10 19:01:09 -0400

    end end day 1

--- src/cs3500/animator/Excellence.java
+++ src/cs3500/animator/Excellence.java
@@ -1,8 +1,12 @@
 package cs3500.animator;
 
+import java.io.InputStreamReader;
+import cs3500.animator.model.Model;
+import cs3500.animator.util.AnimationReader;
+
 public class Excellence {
   public static void main(String [] args) {
-    
+    AnimationReader.parseFile(new InputStreamReader(System.in), new Model.Builder());
   }
 
 }
