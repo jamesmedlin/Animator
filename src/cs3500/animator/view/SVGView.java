5117f83ea96efc2641dad5551a1a19f0c396218e
medlinj <medlinj@rose-hulman.edu>
2019-06-11 18:08:24 -0500

    help meeee

--- src/cs3500/animator/view/SVGView.java
+++ src/cs3500/animator/view/SVGView.java
@@ -41,55 +41,10 @@ public class SVGView implements IView {
     String result = "";
     List<IReadOnlyAnimatedShape> shapes = model.getShapes();
     for (IReadOnlyAnimatedShape shape : shapes) {
-      if (shape.getType() == ShapeType.RECTANGLE) {
-      result += "<" + convertType(shape.getType()) + " id=\""
-              + shape.getName() + "\" x=\"" + shape.getStates().get(0).getPosition().getX()
-              + "\"  y=\"" + shape.getStates().get(0).getPosition().getY() + "\" width=\""
-              + shape.getStates().get(0).getWidth() + "\" height=\""
-              + shape.getStates().get(0).getHeight() + "\" fill=\"rgb("
-              + shape.getStates().get(0).getColor().getRed() + ","
-              + shape.getStates().get(0).getColor().getGreen() + ","
-              + shape.getStates().get(0).getColor().getBlue() + ")\" visibility=\"visible\" >";
-      }
-      if (shape.getType() == ShapeType.ELLIPSE) {
-        result += "<" + convertType(shape.getType()) + " id=\""
-                + shape.getName() + "\" cx=\"" + shape.getStates().get(0).getPosition().getX()
-                + "\"  cy=\"" + shape.getStates().get(0).getPosition().getY() + "\" rx=\""
-                + shape.getStates().get(0).getWidth() / 2 + "\" ry=\""
-                + shape.getStates().get(0).getHeight() / 2 + "\" fill=\"rgb("
-                + shape.getStates().get(0).getColor().getRed() + ","
-                + shape.getStates().get(0).getColor().getGreen() + ","
-                + shape.getStates().get(0).getColor().getBlue() + ")\" visibility=\"visible\" >";
-      }
-      result += stateConverter(shape.getStates());
-      result += "</" + convertType(shape.getType()) + ">\n";
     }
     return result;
   }
 
 
-  private static String convertType(ShapeType type) {
-    switch (type) {
-      case RECTANGLE:
-        return "rect";
-      case ELLIPSE:
-        return "ellipse";
-      default:
-        throw new IllegalArgumentException("Must be a valid type");
-    }
-  }
 
-  private String stateConverter(List<IReadOnlyShapeState> states) {
-    String result = "";
-    for (int i = 0; i < states.size() - 1; i += 2) {
-      if (states.get(i).getPosition().getX() != states.get(i + 1).getPosition().getX()) {
-        result += "<animate attributesType=\"xml\" begin=\"" +
-                (states.get(i).getTick() / this.rate * 1000) + "ms\" dur=\""
-                + ((states.get(i + 1).getTick() - states.get(i).getTick()) / this.rate * 1000)
-                + "ms\" attributeName=\"x\" from=\"" + states.get(i).getPosition().getX()
-                + "\" to=\"" + states.get(i + 1).getPosition().getX() + "\" fill=\"freeze\" />";
-      }
-
-      }
-  }
 }
